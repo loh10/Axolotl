2df2d2b9b7c39e86a3fc90ce11268042a71cc382
Loh <naturellement.toi@gmail.com>
2025-07-31 18:35:27 +0200

    Add text mesh pro

---
+++ Assets/Entity/BaseRole/Wolf.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Wolf : Role
+{
+    protected override void NightAction()
+    {
+        return ;
+    }
+
+    protected override void DayAction()
+    {
+        return;
+    }
+}
